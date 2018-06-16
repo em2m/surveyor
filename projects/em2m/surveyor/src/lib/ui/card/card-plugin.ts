8d60bc8f4115517ee576f9ab5f2c4284c37d7726
Julien Cruz <julien.cruz@gmail.com>
2018-06-16 13:30:10 -0400

    Added new grid cardboard to the card plugin

--- projects/em2m/surveyor/src/lib/ui/card/card-plugin.ts
+++ projects/em2m/surveyor/src/lib/ui/card/card-plugin.ts
@@ -5,6 +5,8 @@ import {StandardCardBoardRenderer} from "./renderers/cardboard/standard/standard
 import {StackCardBoardRenderer} from "./renderers/cardboard/stack/stack-card-board.component";
 import {AccordionCardBoardRenderer} from "./renderers/cardboard/accordion/accordion-card-board.component";
 import {CollapsibleCardComponent} from "./renderers/card/collapsible/collapsible-card.component";
+import {GridCardBoardRenderer} from "./renderers/cardboard/grid/grid-card-board.component";
+import {PlainCardComponent} from "./renderers/card/plain/plain-card.component";
 
 export let CardPlugin: Plugin = <Plugin> {
   name: "Card Plugin",
@@ -12,12 +14,14 @@ export let CardPlugin: Plugin = <Plugin> {
     "surveyor:card-renderer": [
       {value: CollapsibleCardComponent, config: {type: "collapsible"}},
       {value: StandardCardComponent, config: {type: "standard"}},
-      {value: StackCardComponent, config: {type: "stack"}}
+      {value: StackCardComponent, config: {type: "stack"}},
+      {value: PlainCardComponent, config: {type: "plain"}}
     ],
     "surveyor:card-board-renderer": [
       {value: AccordionCardBoardRenderer, config: {type: "accordion"}},
       {value: StandardCardBoardRenderer, config: {type: "standard"}},
-      {value: StackCardBoardRenderer, config: {type: "stack"}}
+      {value: StackCardBoardRenderer, config: {type: "stack"}},
+      {value: GridCardBoardRenderer, config: {type: "grid"}}
     ]
   }
 };
---
+++ projects/em2m/surveyor/src/lib/ui/card/renderers/cardboard/grid/grid-card-board.component.ts
@@ -0,0 +1,37 @@
+import {Component, EventEmitter, OnChanges, OnInit, Output} from "@angular/core";
+import {CardService} from "../../../card.service";
+import {Card} from "../../../card.model";
+import {CardBoardRenderer} from "../card-board.renderer";
+
+@Component({
+  selector: 'surveyor-grid-card-board',
+  templateUrl: './grid-card-board.component.html',
+  styleUrls: ['./grid-card-board.component.scss']
+})
+export class GridCardBoardRenderer extends CardBoardRenderer implements OnInit, OnChanges {
+
+  @Output() visible = new EventEmitter();
+
+  public cards: Array<Card> = [];
+
+  constructor(private cardService: CardService) {
+    super();
+  }
+
+  ngOnInit() {
+    this.updateCards();
+  }
+
+  ngOnChanges() {
+    this.updateCards();
+  }
+
+  updateCards() {
+    this.cards = this.cardService.findCards(this.target);
+  }
+
+  created(card) {
+    this.cardObs.next(card);
+  }
+
+}
