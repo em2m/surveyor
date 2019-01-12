4cdbe7a43f8cf8585492422e3e11b22d6bf378d2
Tkaldahl <tkaldahl90@gmail.com>
2019-01-12 15:33:32 -0500

    update package.lock and email validator

--- projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
+++ projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
@@ -4,7 +4,7 @@ export class SurveyorValidators {
 
   static isValidEmailFormat(c: AbstractControl) {
     let EMAIL_REGEXP = /\S+@\S+\.\S+/;
-    if (c.value && (c.value.length <= 5 || !EMAIL_REGEXP.test(c.value))) {
+    if (c.value && (c.value.length <= 4 || !EMAIL_REGEXP.test(c.value))) {
       return {invalidEmail: true};
     } else {
       return null;
