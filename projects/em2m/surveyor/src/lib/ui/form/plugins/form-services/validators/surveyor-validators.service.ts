6550232c856a62a874b08bf1fa6f4b6503b2bb80
Tkaldahl <tkaldahl90@gmail.com>
2019-01-15 15:37:12 -0500

    edit token validator

--- projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
+++ projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
@@ -22,7 +22,7 @@ export class SurveyorValidators {
 
   static isValidToken(c: AbstractControl) {
     let tokenRegex = /[a-z0-9]{6}/gi;
-    if (c.value && (c.value.length !== 6 || !tokenRegex.test(c.value))) {
+    if (c.value && !tokenRegex.test(c.value)) {
       return {invalidToken: true};
     } else {
       return null;
