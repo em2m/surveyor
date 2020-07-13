a409163a040784fdd070f49d7dbbd87de63ec078
Julien Cruz <julien.cruz@gmail.com>
2020-07-13 06:16:48 -0700

    Disabled apple maps plugins due to issues with angular 9 compilation

--- projects/em2m/surveyor/src/lib/ui/leaflet/providers/apple-mapkit-js/apple-mapkit.provider.ts
+++ projects/em2m/surveyor/src/lib/ui/leaflet/providers/apple-mapkit-js/apple-mapkit.provider.ts
@@ -5,50 +5,57 @@ import {AppConfig} from '../../../../core/config/config.service';
 import 'apple-mapkit-js';
 import 'leaflet.mapkitmutant';
 import {ContextService} from '../../../../core/extension/context.service';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {AppleMapkitLoaderService} from './apple-mapkit-loader.service';
 
 @Injectable()
 export class AppleMapkitProvider extends LayerProvider {
 
   config: any;
 
-  constructor(private appConfig: AppConfig, private ctx: ContextService) {
+  constructor(private appConfig: AppConfig, private ctx: ContextService/*, private appleMaps: AppleMapkitLoaderService*/) {
     super();
   }
 
-  provide(): Array<LayerDefinition> | Array<LayerDefinition> {
+  provide(): Array<LayerDefinition> | Observable<Array<LayerDefinition>> {
     this.resolveProvider(this.appConfig, this.ctx);
 
     if (this.mapProvider === 'apple') {
       const accessToken = this.mapConfig.appleKey || this.mapConfig.token;
 
-      const streetsLayer = <LayerDefinition>{
-        label: 'Streets',
-        layer: (L as any).mapkitMutant({
-          type: 'standard',
-          minZoom: 3,
-          authorizationCallback: (done) => done(accessToken)
-        })
-      };
-      /*
-      const hybridLayer = <LayerDefinition>{
-        label: 'Hybrid',
-        layer: (L as any).mapkitMutant({
-          type: 'hybrid',
-          minZoom: 3,
-          authorizationCallback: (done) => done(accessToken)
-        })
-      };
-      */
-      const satelliteLayer = <LayerDefinition>{
-        label: 'Satellite',
-        layer: (L as any).mapkitMutant({
-          type: 'hybrid',
-          minZoom: 3,
-          authorizationCallback: (done) => done(accessToken)
-        })
-      };
+      //return this.appleMaps.loadApi().pipe(
+      //  map(() => {
+          const streetsLayer = {
+            label: 'Streets',
+            layer: (L as any).mapkitMutant({
+              type: 'standard',
+              minZoom: 3,
+              authorizationCallback: (done) => done(accessToken)
+            })
+          } as LayerDefinition;
+          /*
+          const hybridLayer = {
+            label: 'Hybrid',
+            layer: (L as any).mapkitMutant({
+              type: 'hybrid',
+              minZoom: 3,
+              authorizationCallback: (done) => done(accessToken)
+            })
+          } as LayerDefinition;
+          */
+          const satelliteLayer = {
+            label: 'Satellite',
+            layer: (L as any).mapkitMutant({
+              type: 'hybrid',
+              minZoom: 3,
+              authorizationCallback: (done) => done(accessToken)
+            })
+          } as LayerDefinition;
 
-      return [satelliteLayer, streetsLayer];
+          return [satelliteLayer, streetsLayer];
+      //  })
+      //);
     } else {
       return [];
     }
