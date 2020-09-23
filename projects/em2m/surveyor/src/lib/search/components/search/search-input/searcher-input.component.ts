49e146e61e5c3f16f2ebc4978b9a8ee5c20849c5
Christian  Villamor <christian@ip-192-168-1-178.ec2.internal>
2020-09-23 09:47:42 -0400

    Fix searcher query

--- projects/em2m/surveyor/src/lib/search/components/search/search-input/searcher-input.component.ts
+++ projects/em2m/surveyor/src/lib/search/components/search/search-input/searcher-input.component.ts
@@ -42,7 +42,11 @@ export class SearcherInputComponent implements OnInit {
         this.searcher.fullTextFields.forEach(field => {
           const innerQueries = [];
           tokenizedSearchInput.forEach(queryString => {
-            innerQueries.push(new LuceneQuery(`${field}:*${queryString}*`, field));
+            if (isNaN(Number(queryString))) {
+              innerQueries.push(new LuceneQuery(`${field}:*${queryString}*`, field));
+            } else {
+              innerQueries.push(new LuceneQuery(`${field}:*\"${queryString}\"*`, field));
+            }
           });
           outerQueries.push(new BoolQuery(OperationType.AND, innerQueries));
         });
