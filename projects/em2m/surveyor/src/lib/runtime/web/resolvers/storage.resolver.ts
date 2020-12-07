6838a98d84aa67b1d6451a36c7bc784b82b6b54d
Julien Cruz <julien.cruz@gmail.com>
2020-12-07 09:14:02 -0800

    Updated surveyor to load values from local and session storage as needed when the local context service does not yet have the value

---
+++ projects/em2m/surveyor/src/lib/runtime/web/resolvers/storage.resolver.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Router, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {ContextService} from '../../../core/extension/context.service';
+
+@Injectable({ providedIn: 'root' })
+export class WebStorageResolver implements Resolve<any> {
+
+  constructor(private router: Router,
+              private ctx: ContextService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<any> {
+    // Load Local Storage variables into the context service
+    for (const key of Object.keys(localStorage)) {
+      const value = localStorage.getItem(key);
+      this.ctx.setValue(key, JSON.parse(value), { broadcast: false, storage: 'NONE' });
+    }
+
+    // Load Local Storage variables into the context service
+    for (const key of Object.keys(sessionStorage)) {
+      const value = sessionStorage.getItem(key);
+      this.ctx.setValue(key, JSON.parse(value), { broadcast: false, storage: 'NONE' });
+    }
+  }
+}
