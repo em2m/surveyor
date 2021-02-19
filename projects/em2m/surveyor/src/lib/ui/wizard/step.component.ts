4cd5f7853f00079d4604efb75329971807c5b9a1
Ryan Krolick <ryan.krolick@elasticm2m.com>
2021-02-19 17:21:20 -0500

    update to allow onStepCompleted to wizard/step components

--- projects/em2m/surveyor/src/lib/ui/wizard/step.component.ts
+++ projects/em2m/surveyor/src/lib/ui/wizard/step.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'surveyor-step',
@@ -8,6 +9,7 @@ import {Component, Input} from '@angular/core';
 export class SurveyorWizardStepComponent {
 
   animateClass: string;
+  @Input() onStepCompleted: Observable<boolean> = null;
   @Input() active = false;
   @Input() loaded = false;
   @Input() skipped = false;
