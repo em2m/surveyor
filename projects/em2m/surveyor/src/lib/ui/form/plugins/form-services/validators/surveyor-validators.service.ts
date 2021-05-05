f43aac148e48c560ca39873b1f5059c6df6ba76f
Tetyana Radu <tetyanaradu@ip-192-168-100-8.ec2.internal>
2021-05-05 09:29:04 -0400

    update email validator to support complex emails

--- projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
+++ projects/em2m/surveyor/src/lib/ui/form/plugins/form-services/validators/surveyor-validators.service.ts
@@ -4,7 +4,7 @@ import has = Reflect.has;
 export class SurveyorValidators {
 
   static isValidEmailFormat(c: AbstractControl) {
-    const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const EMAIL_REGEXP = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     if (c.value && !EMAIL_REGEXP.test(c.value)) {
       return {invalidEmail: true};
     } else {
