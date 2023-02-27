fd08c085c183589b1cf9796833d8e0e0898c2dea
tkaldahl <tkaldahl90@gmail.com>
2023-02-27 10:39:57 -0500

    add support for unit conversion

---
+++ projects/em2m/surveyor/src/lib/ui/converter/converter.component.ts
@@ -0,0 +1,11 @@
+export class Converter {
+  constructor() {
+  }
+
+  fromSavedUnits(value: number): number {
+    return value;
+  }
+  toSavedUnits(value: number): number {
+    return value;
+  }
+}
