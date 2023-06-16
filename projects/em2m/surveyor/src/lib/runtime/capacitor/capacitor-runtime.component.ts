342c369fe994107165c36bcb9368e6c93c7d808b
ivo <ivailo96g@gmail.com>
2023-06-16 17:15:24 -0400

    [Feat] added Splash Screen plugin, export network service

--- projects/em2m/surveyor/src/lib/runtime/capacitor/capacitor-runtime.component.ts
+++ projects/em2m/surveyor/src/lib/runtime/capacitor/capacitor-runtime.component.ts
@@ -1,13 +1,15 @@
-import {Component, ViewContainerRef} from '@angular/core';
+import {AfterViewInit, Component, ViewContainerRef} from '@angular/core';
 import {PopoverService} from '../../ui/popover/popover.service';
 import {ModalService} from '../../ui/modal/modal.service';
 import {DeeplinksService} from "./deeplinks/DeeplinksService";
+import {Capacitor} from '@capacitor/core';
+import {SplashScreen} from '@capacitor/splash-screen';
 
 @Component({
   selector: 'surveyor-runtime',
   template: '<div mdl style="height: 100%; display: flex; flex-direction: column;"><router-outlet></router-outlet></div>'
 })
-export class SurveyorCapacitorRuntime {
+export class SurveyorCapacitorRuntime implements AfterViewInit{
 
   constructor(private viewContainerRef: ViewContainerRef,
               private modalService: ModalService,
@@ -19,10 +21,12 @@ export class SurveyorCapacitorRuntime {
     this.modalService.setRootViewContainerRef(viewContainerRef);
     this.popoverService.setRootViewContainerRef(viewContainerRef);
 
-    /*
+
+  }
+
+  ngAfterViewInit(){
     if (Capacitor.isPluginAvailable('SplashScreen')) {
       SplashScreen.hide();
     }
-     */
   }
 }
