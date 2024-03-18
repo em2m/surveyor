117ad8c958b0d51183b0a1e5888c9f7caa424ba9
Ryan Krolick <ryankrolick@latm0003.lan>
2024-03-18 11:23:56 -0400

    404 page will allow refresh if displayed in mobile app

--- projects/em2m/surveyor/src/lib/runtime/capacitor/application/404/404.page.ts
+++ projects/em2m/surveyor/src/lib/runtime/capacitor/application/404/404.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ExtensionService} from '../../../../core/extension/extension.service';
+import {AppConfig} from "../../../../core/config/config.service";
 
 @Component({
   templateUrl: './404.page.html'
@@ -7,16 +8,23 @@ import {ExtensionService} from '../../../../core/extension/extension.service';
 export class Capacitor404Page implements OnInit {
 
   links: Array<any> = [];
+  isMobile = !!this.config.get().mobile;
 
-  constructor(private extensionService: ExtensionService) {
+  constructor(private extensionService: ExtensionService,
+              private config: AppConfig) {
   }
 
   ngOnInit() {
+    console.log(this.isMobile)
     this.links = this.extensionService.getExtensionsForTypeAndTarget('surveyor:link-contribution', '404')
       .map(extension => {
         if (extension) {
           return extension.value;
         }
       });
+  }
+
+  refresh() {
+    window.location.href = '/';
   }
 }
