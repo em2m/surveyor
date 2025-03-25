8e5ce5013e214ac943f5093cf126703ebf933de1
KyleLynnRas <kylelynn.ras@gmail.com>
2025-03-25 15:55:29 -0400

    move translation to action.service, some template specific translations still done unrelated to action

--- projects/em2m/surveyor/src/lib/ui/action/action.service.ts
+++ projects/em2m/surveyor/src/lib/ui/action/action.service.ts
@@ -2,13 +2,16 @@ import {Injectable, Injector, Type} from '@angular/core';
 import {Action} from './action.model';
 import {ExtensionService} from '../../core/extension/extension.service';
 import {Extension} from '../../core/extension/extension.model';
+import {Surveyori18nService} from "../i18n/shared/i18n.service";
 
 @Injectable()
 export class ActionService {
 
   private ACTION_EXTENSION_TYPE = 'surveyor:action';
 
-  constructor(private injector: Injector, private extensionService: ExtensionService) {
+  constructor(private injector: Injector,
+              private extensionService: ExtensionService,
+              private i18nService: Surveyori18nService) {
   }
 
   findActions(target: string): Array<Action> {
@@ -17,9 +20,12 @@ export class ActionService {
     this.extensionService.getExtensionsForTypeAndTarget(this.ACTION_EXTENSION_TYPE, target)
       .forEach((extension: Extension) => {
         const type = extension.value as Type<Action>;
-        const instance: Action = this.injector.get(type);
+        let instance: Action = this.injector.get(type);
+        instance.text = this.i18nService.translate(instance.text);
+        instance.toolTipText = this.i18nService.translate(instance.toolTipText);
         results.push(instance);
       });
+
     return results;
   }
 
@@ -28,7 +34,10 @@ export class ActionService {
     let ext = this.extensionService.getExtensionById(this.ACTION_EXTENSION_TYPE, id);
     if (ext) {
       const type = ext.value as Type<Action>;
-      result = this.injector.get(type)
+      let instance: Action = this.injector.get(type);
+      instance.text = this.i18nService.translate(instance.text);
+      instance.toolTipText = this.i18nService.translate(instance.toolTipText);
+      result = instance;
     }
     return result;
   }
